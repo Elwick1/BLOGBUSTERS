96e9ede70cbf397419109ac4d1feceb51b4d8d39
Michael Williamson <michaelrwilliamson92@gmail.com>
2019-10-24 13:53:09 +0100

    minor user index changes

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
   belongs_to :user
-  # has_many :users, :through likes
+  has_many :likes, through: :users
   validates :title, presence: true
   validates :content, length: { minimum: 50 }
   validates :content, presence: true 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   has_many :articles
-  has_many :likes 
-  # has_many :liked, :through likes
+  has_many :likes, through: :articles
   validates :username, uniqueness: true
   validates :username, presence: true
   validates :username, length: { maximum: 15 }
